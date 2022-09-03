2c7acde619692cfb7a495095fcb716fa1ab4bb2c
Kai Haberland <kai.haberland2@gmail.com>
2022-09-03 11:16:49 +0200

    Creation Repo with Main-file and database_handler class.

---
+++ Main.java
@@ -0,0 +1,11 @@
+package cad_lib;
+
+public class Main {
+    
+    public static void main(String[] args)
+    {
+        database_handler db_handler = new database_handler();
+
+        System.out.println("Erstellt");
+    }
+}
